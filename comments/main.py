8d3c15f9d70ecf7f01b649eeb4118955b4142f89
Paula-Reisen <36951747+Paula-Reisen@users.noreply.github.com>
2022-04-24 15:09:39 +0200

    Sync 24-4-22

---
+++ comments/main.py
@@ -0,0 +1,18 @@
+# Do not modify these lines
+__winc_id__ = '63ce21059cf34d3d8ffef497ede7e317'
+__human_name__ = 'comments'
+
+# Creating Mutiple Variables with a calculation for age
+# This seems fair
+""" So we have the average age of everyone 
+    Without debatting who is older then whom"""
+""" They only differ a few months from each other """
+
+AgePaula = 31
+AgeCasper = 31
+AgeTessa = 31  # everyone is the same age
+AvgAge = AgeCasper + AgePaula + AgeTessa / 3  # average age of everyone
+
+waltz = 'onetwothree' + str(4)
+print(waltz)
+
---
+++ hello-world/main.py
@@ -0,0 +1,11 @@
+# Do not modify these lines
+__winc_id__ = 'e75b6cd4a7404e3ca76c308566dafb5d'
+__human_name__ = 'hello-world'
+
+# Add your code after this line
+print('Hi, my name is Mark!')
+print('Python can waltz!')
+print('one', 'two', 'three')
+
+print(16)
+print(1, 2, 3)
