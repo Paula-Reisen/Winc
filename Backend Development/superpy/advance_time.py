df63648baab58ba1798a332020faa6f3cf90ff0b
Paula-Reisen <36951747+Paula-Reisen@users.noreply.github.com>
2022-09-25 17:14:31 +0200

    changes to superpy

--- Backend Development/superpy/advance_time.py
+++ Backend Development/superpy/advance_time.py
@@ -4,6 +4,5 @@ import handle_date
 def advanceTime(IntDays):
     advanced_date = datetime.datetime.now() + datetime.timedelta(days=IntDays)
     advanced_date.date()
-    #handle_date.handle_date(advanced_date)
     return advanced_date
     
