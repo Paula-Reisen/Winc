f29d9e64b1a64211747f56c84a7fc14dadf7bcfa
Paula-Reisen <36951747+Paula-Reisen@users.noreply.github.com>
2022-10-08 11:23:24 +0200

    Delete Backend Development/superpy directory

--- Backend Development/superpy/advance_time.py
+++
@@ -1,8 +0,0 @@
-import datetime
-import handle_date
-
-def advanceTime(IntDays):
-    advanced_date = datetime.datetime.now() + datetime.timedelta(days=IntDays)
-    advanced_date.date()
-    return advanced_date
-    
--- Backend Development/superpy/resetFilesForNextUse.py
+++
@@ -1,2 +0,0 @@
-def resetFilesForNextUse(): 
-    print
